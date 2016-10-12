bfe7fc05b6b6c52d40581431f0447ab255057a02
DangFiztssi <ngdang31@gmail.com>
2016-10-12 20:20:51 +0700

    Revert "FB"
    This reverts commit bb12a964c3ada86a0fdea96c48a63f13d01a519e.

--- app/src/main/java/apidez/com/lab1/activity/LoginActivity.java
+++ app/src/main/java/apidez/com/lab1/activity/LoginActivity.java
@@ -1,41 +1,18 @@
 package apidez.com.lab1.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import apidez.com.lab1.R;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private TextView tvSignup, tvForgotPass;
-    private EditText edtEmail, edtPass;
-    private Button btnLogin;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        tvSignup = (TextView) findViewById(R.id.tv_signup);
-        tvForgotPass=(TextView) findViewById(R.id.tv_forgot);
-
-        edtEmail = (EditText) findViewById(R.id.edt_username);
-        edtPass = (EditText) findViewById(R.id.edt_password);
-
-        btnLogin = (Button) findViewById(R.id.btn_login);
-
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, StreamActivity.class));
-            }
-        });
+        // TODO: Insert your code
     }
 
     private void showToast(String message) {
--- app/src/main/java/apidez/com/lab1/adapter/PostAdapter.java
+++ app/src/main/java/apidez/com/lab1/adapter/PostAdapter.java
@@ -2,18 +2,14 @@ package apidez.com.lab1.adapter;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.squareup.picasso.Picasso;
-
 import java.util.List;
 
-import apidez.com.lab1.R;
 import apidez.com.lab1.model.Post;
 
 /**
@@ -36,36 +32,18 @@ public class PostAdapter extends ArrayAdapter<Post> {
     @NonNull
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        ViewHolder viewHolder;
-
-        if(convertView == null){
-            convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_post,parent,false);
-            viewHolder = new ViewHolder(convertView);
-            convertView.setTag(viewHolder);
-        }
-        else{
-            viewHolder = (ViewHolder)convertView.getTag();
-        }
-        bindViewHolder(position, viewHolder);
+        // TODO: Insert your code
+        // bindViewHolder(position, viewHolder)
         return convertView;
     }
 
     private void bindViewHolder(int position, ViewHolder viewHolder) {
         Post post = mPosts.get(position);
-
-        viewHolder.tvUsername.setText(post.getUsername());
-        viewHolder.tvDate.setText(post.getDate());
-        viewHolder.tvDescription.setText(post.getDescription());
-
-        loadImage(viewHolder.ivAvatar, post.getAvatar());
-        loadImage(viewHolder.ivImage, post.getImage());
+        // TODO: Insert your code
     }
 
     private void loadImage(ImageView imageView, String path) {
-        Picasso.with(getContext())
-                .load(path)
-                .placeholder(R.drawable.placeholder)
-                .into(imageView);
+        // TODO: Insert your code
     }
 
     public class ViewHolder {
@@ -74,13 +52,5 @@ public class PostAdapter extends ArrayAdapter<Post> {
         public TextView tvDate;
         public ImageView ivAvatar;
         public ImageView ivImage;
-
-        public ViewHolder(View viewItem) {
-            tvUsername = (TextView) viewItem.findViewById(R.id.tvUsername);
-            tvDescription = (TextView) viewItem.findViewById(R.id.tvDescription);
-            tvDate = (TextView) viewItem.findViewById(R.id.tvDate);
-            ivAvatar = (ImageView) viewItem.findViewById(R.id.ivAvatar);
-            ivImage = (ImageView) viewItem.findViewById(R.id.ivImage);
-        }
     }
 }
